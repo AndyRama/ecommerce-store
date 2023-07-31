c4998388fc9a2270628fa8a13a96ec8087872130
Andy Ramaroson <andyramaroson@gmail.com>
2023-07-31 12:16:13 +0200

    [enhancement] update types for product, get-billboard (work) and display in home page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,13 +1,13 @@
-import { Container } from "lucide-react"
-import Billboard from "../components/billboard"
+import getBillboard from "@/actions/get-billboard"
+import Container  from "../components/ui/container"
+import Billboard from "../components/billboard";
 
-const HomePage= () => {
+const HomePage = async () => {
+  const billboard = await getBillboard("b0baf98d-9f14-429b-a99c-bd5b737134d7")
   return (
     <Container>
-      <div 
-        className="space-y-10 pb-10"
-      >
-        <Billboard data={""}/>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard}/>
       </div>
     </Container>
   )
