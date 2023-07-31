e5284fb1543c1e709c4ae3924c0ef031f6932a40
Andy Ramaroson <andyramaroson@gmail.com>
2023-07-31 22:19:26 +0200

    [enhancement] add description for cards and create currency with formatteur for price

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -9,14 +9,14 @@ export const revalidate = 0;
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true})
   const billboard = await getBillboard("b0baf98d-9f14-429b-a99c-bd5b737134d7")
+
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard data={billboard}/>
-      </div>
-
-      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-        <ProductList title="Featured Products" items={products}/>
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products}/>
+        </div>
       </div>
     </Container>
   )
--- app/components/ui/product-card.tsx
+++ app/components/ui/product-card.tsx
@@ -5,6 +5,7 @@ import { Expand, ShoppingCart } from "lucide-react"
 
 import { Product } from '@/types'
 import IconButton from '@/app/components/ui/icon-button'
+import Currency from './currency'
 
 interface ProductCard {
   data: Product
@@ -24,15 +25,28 @@ const ProductCard: React.FC<ProductCard> = ({
               className='aspect-square object-cover rounded-md'
             />
             <div className='opacity-0 group-hover:opacity-100 transistion absolute w-full px-6 bottom-5'>
-                <div className='flex gap-x-6 justify-center'>
-                  <IconButton onClick={() => { } } 
-                    icon={<Expand size={20} className='text-gray-600'/>}
-                  />
-                  <IconButton onClick={() => { } } 
-                    icon={<ShoppingCart size={20} className='text-gray-600'/>}
-                  />
-                </div>
+              <div className='flex gap-x-6 justify-center'>
+                <IconButton onClick={() => { } } 
+                  icon={<Expand size={20} className='text-gray-600'/>}
+                />
+                <IconButton onClick={() => { } } 
+                  icon={<ShoppingCart size={20} className='text-gray-600'/>}
+                />
+              </div>
             </div>
+          </div>
+          {/* Description */}
+          <div>
+            <p className='font-semibold text-lg'>
+              {data.name}
+            </p>
+            <p className='text-sm text-gray-500'>
+              {data.category?.name}
+            </p>
+          </div>
+          {/* Price */}
+          <div className='flex items-center justify-between'>
+            <Currency value={data?.price} />
           </div>
       </div>
   )
