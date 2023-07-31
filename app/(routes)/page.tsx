18d34ab1408db1f5d898411c17342e385fb3dbd7
Andy Ramaroson <andyramaroson@gmail.com>
2023-07-31 18:09:01 +0200

    [enhancement] add productList and create No-result

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -2,6 +2,9 @@ import getBillboard from "@/actions/get-billboard"
 import Container  from "../components/ui/container"
 import Billboard from "../components/billboard";
 import getProducts from "@/actions/get-products";
+import ProductList from "../components/product-list";
+
+export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true})
