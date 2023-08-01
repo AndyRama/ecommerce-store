ffcfa64f3bab639aeb9413da07ec32966e8e69ae
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-01 16:05:51 +0200

    [enhancement] create Gallery for products

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,5 +1,6 @@
 import getProduct from "@/actions/get-product"
 import getProducts from "@/actions/get-products"
+import Gallery from "@/app/components/gallery"
 import ProductList from "@/app/components/product-list"
 import Container from "@/app/components/ui/container"
 
@@ -16,12 +17,13 @@ const ProductPage: React.FC<ProductPageProps> = async ({
   const suggestedProducts = await getProducts({
     categoryId: product?.category?.id
   })
+
   return (
     <div className="bg-white"> 
       <Container>
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-            {/* Galery */}
+            <Gallery images={product.images} />
             <div>Galery</div>
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
               {/* Info */}
@@ -29,7 +31,7 @@ const ProductPage: React.FC<ProductPageProps> = async ({
             </div>
           </div>
           <hr className="my-10"/>
-          <ProductList title="Related Items" items={suggestedProducts }/>
+          <ProductList title="Related Items" items={ suggestedProducts }/>
         </div>
       </Container>  
     </div>
