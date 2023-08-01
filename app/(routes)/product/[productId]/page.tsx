9e7f8c0d5b8112aaf8a3ff32374a6ef14e17dc03
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-01 11:50:28 +0200

    [enhancement] update page home and create get-product

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,4 +1,18 @@
-const ProductPage = () => {
+import getProducts from "@/actions/get-products"
+
+interface ProductPageProps {
+  params: {
+    productId: string
+  }
+}
+
+const ProductPage: React.FC<ProductPageProps> = async ({
+  params
+}) => {
+  const Product = {}
+  const suggestedProducts =await getProducts({
+    categoryId: Product?.category?.id
+  })
   return (
     <div> 
       Indivdual Product
