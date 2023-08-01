cd4d9778f2733c42541d72436cdb7f51e7c96cb0
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-01 17:46:50 +0200

    [enhancement] create components GalleryTab

--- app/components/gallery/index.tsx
+++ app/components/gallery/index.tsx
@@ -5,6 +5,8 @@ import { Tab } from '@headlessui/react'
 
 import { Image as ImageType } from "@/types"
 
+import GalleryTab  from './gallery-tab'
+
 interface GalleryProps {
   images: ImageType[]
 }
@@ -17,7 +19,7 @@ const Gallery: React.FC<GalleryProps> = ({
       <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
         <Tab.List className="gird grid-cols-4 gap-6 ">
           { images.map((image) => {
-            <GalleryTab />
+            <GalleryTab key={image.id} image={image}/>
           })}
         </Tab.List>
       </div>
