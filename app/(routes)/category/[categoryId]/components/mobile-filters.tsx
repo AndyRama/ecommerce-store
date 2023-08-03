82c7f728eb47a45297746469cb391add457f8f8e
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-03 18:24:58 +0200

    [Enhancement]  add category for modalPanel dialog

--- app/(routes)/category/[categoryId]/components/mobile-filters.tsx
+++ app/(routes)/category/[categoryId]/components/mobile-filters.tsx
@@ -1,14 +1,18 @@
 "use client"
 
-import Button from "@/app/components/ui/button"
-import { Color, Size } from "@/types"
-import { Dialog } from "@headlessui/react"
-import { Plus } from "lucide-react"
 import { useState } from "react"
+import { Plus, X } from "lucide-react"
+import { Dialog } from "@headlessui/react"
+
+import { Color, Size } from "@/types"
+import Button from "@/app/components/ui/button"
+import IconButton from "@/app/components/ui/icon-button"
+import Filter from "./filter"
+
 
 
 interface MobileFiltersProps {
-  sizes: Size[]
+  sizes: Size[],
   colors: Color[]
 }
 const MobileFilters: React.FC<MobileFiltersProps> = ({
@@ -33,9 +37,26 @@ const MobileFilters: React.FC<MobileFiltersProps> = ({
 
         {/* Dialog position  */}
         <div className="fixed inset-0 z-40 flex">
-          <Dialog.Panel className=" relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl" >
+          <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
+            
+            {/* Close button */}
+            <div className="flex items-center justify-end px-4">
+              <IconButton icon={<X size={15} />} onClick={onClose} />
+            </div>
 
-          </Dialog.Panel>       
+            <div className="p-4">
+              <Filter
+                valueKey="sizeId" 
+                name="Sizes" 
+                data={sizes}
+              />
+              <Filter 
+                valueKey="colorId" 
+                name="Colors" 
+                data={colors}
+              />
+            </div>
+          </Dialog.Panel>    
         </div>
       </Dialog>
     </>
