b8c980e00dad711e19833058ef70025f68084bcd
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-03 16:02:36 +0200

    [Enhancement]  add backgroud   when click btn filters

--- app/(routes)/category/[categoryId]/components/mobile-filters.tsx
+++ app/(routes)/category/[categoryId]/components/mobile-filters.tsx
@@ -2,6 +2,7 @@
 
 import Button from "@/app/components/ui/button"
 import { Color, Size } from "@/types"
+import { Dialog } from "@headlessui/react"
 import { Plus } from "lucide-react"
 import { useState } from "react"
 
@@ -22,12 +23,21 @@ const MobileFilters: React.FC<MobileFiltersProps> = ({
 
   return (
     <>
-    <Button onClick={onOpen} className="flex items-center gap-x-2 lg:hidden">
-      Filters
-      <Plus size={20} />
-    </Button>
-    
-    
+      <Button onClick={onOpen} className="flex items-center gap-x-2 lg:hidden">
+        Filters
+        <Plus size={20} />
+      </Button> 
+      <Dialog open={open} as="div" className="relative z-40 lg:hiden" onClose={onClose} >
+        {/* Background */}
+        <div className="fixed inset-0 bg-black bg-opacity-25" />
+
+        {/* Dialog position  */}
+        <div className="fixed inset-0 z-40 flex">
+          <Dialog.Panel className=" relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl" >
+
+          </Dialog.Panel>       
+        </div>
+      </Dialog>
     </>
   )
 }
