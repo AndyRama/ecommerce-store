9d75d7fa35973c0bf67e5a8414efbe0d1f27a850
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-04 21:18:47 +0200

    [Enhancement] Update ModalProvider and create toaster and display in Layout

--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -11,7 +11,7 @@ const ModalProvider = () => {
     setIsMounted(true)
   }, [])
 
-  if    (!isMounted) {
+  if (!isMounted) {
     return null
   }
 
---
+++ providers/toast-provider.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { Toaster } from "react-hot-toast"
+
+Toaster
+const ToastProvider = () => {
+ return (
+  <Toaster />
+ )
+
+}
+export default ToastProvider
