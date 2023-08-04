d1d755e39f87928535c31cd7b16962c606b7cd2e
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-04 12:30:28 +0200

    [Enhancement] add components PreviewsModal

---
+++ app/components/ui/preview-modal.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import usePreviewModal from "@/hooks/use-preview-modal"
+import Modal from "@/app/components/ui/modal"
+import Gallery from "@/app/components/gallery"
+import Info from "@/app/components/info"
+
+const PreviewModal = () => {
+  const previewModal = usePreviewModal()
+  const product = usePreviewModal((state) => state.data)
+
+  if(!product) {
+    return null
+  }
+
+
+  return(
+    <Modal 
+      open={previewModal.isOpen}
+      onClose={previewModal.onClose}
+    >
+      <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8">
+        <div className="sm:col-span-4 lg:col-span-5">
+          <Gallery images={product.images}/>
+        </div>
+      </div>
+      <div className="sm:col-span-8 lg:col-span-7">
+        <Info data={product} />
+
+      </div>     
+
+    </Modal>
+  )
+}
+
+export default PreviewModal
