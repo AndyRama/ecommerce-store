ea15fdd3cdf56f54bb17241c04bedf0822ba06cc
Andy Ramaroson <andyramaroson@gmail.com>
2023-09-18 14:21:24 +0200

    [enhancement] add skeleton for loarding

--- app/components/main-nav.tsx
+++ app/components/main-nav.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link"
 import { usePathname } from "next/navigation";
 
-import { cn } from "../lib/utils"
+import { cn } from "@/app/lib/utils"
 import { Category } from "@/types";
 
 interface MainNavProps {
@@ -11,38 +11,36 @@ interface MainNavProps {
 }
 
 const MainNav: React.FC<MainNavProps> = ({
-  data
+  data,
 }) => {
   const pathname = usePathname();
 
-  const routes = data.map((route) => ({
+  let routes = data.map((route) => ({
     href: `/category/${route.id}`,
     label: route.name,
-    active: pathname === `/category/${route.id}`,
+    active: pathname === `/category/${route.id}`
   }));
-
+  
   return (
     <nav
-      className="mx-6 flex items-center space-x-4 lg:space-x-6"
+    className="mx-6 flex items-center space-x-4 lg:space-x-6"
     >
       {routes.map((route) => (
         <Link
           key={route.href}
-          href={route.href}
+          href={route.href}          
           className={cn(
             'text-sm font-medium transition-colors hover:text-black',
             route.active ? 'text-black' : 'text-neutral-500'
-          )}
-        >
+            )}
+            >
           {route.label}
-         {/* test test test  */}
-          
+          shoes
       </Link>
       ))}
     </nav>
-  )
-
-  
+  )  
 };
+
 
 export default MainNav;
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = async () => {
           <MainNav data={categories} />
           <NavbarActions />
         </div>
-      </Container>
+      </Container>    
     </div>
   );
 };
